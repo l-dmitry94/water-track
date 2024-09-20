5cfd0db238c5469f8e897d17afcf451e907ba102
Dmytro <64677316+l-dmitry94@users.noreply.github.com>
2024-09-20 10:34:52 +0300

    welcome completed

--- components/ui/Container/Container.tsx
+++ components/ui/Container/Container.tsx
@@ -1,12 +1,14 @@
 import { FC, ReactNode } from 'react';
 import scss from './Container.module.scss';
+import clsx from 'clsx';
 
 interface IContainer {
     children: ReactNode;
+    className?: string;
 }
 
-const Container: FC<IContainer> = ({ children }) => {
-    return <div className={scss.container}>{children}</div>;
+const Container: FC<IContainer> = ({ children, className }) => {
+    return <div className={clsx(scss.container, className)}>{children}</div>;
 };
 
 export default Container;
