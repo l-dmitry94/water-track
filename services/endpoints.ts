a07d93490bbaa041d2142389a8f8424e87366cd4
Dmytro <64677316+l-dmitry94@users.noreply.github.com>
2024-09-26 15:03:50 +0300

    update

--- services/endpoints.ts
+++ services/endpoints.ts
@@ -1,13 +1,13 @@
 const ENDPOINTS = {
     auth: {
-        signup: 'auth/signup',
+        signup: '/auth/signup',
     },
 
     waters: {
-        create: '/api/water/create',
-        getDailyWaters: '/api/water/daily',
-        getWeeklyWaters: '/api/water/weekly',
-        getMonthlyWaters: '/api/water/monthly',
+        create: '/water/create',
+        getDailyWaters: '/water/daily',
+        getWeeklyWaters: '/water/weekly',
+        getMonthlyWaters: '/water/monthly',
     },
 };
 
