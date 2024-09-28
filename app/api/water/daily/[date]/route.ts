804d826c499f1e923c96c4e5e1b033b94cd759e4
Dmytro <64677316+l-dmitry94@users.noreply.github.com>
2024-09-28 14:53:03 +0300

    Update route.ts

--- app/api/water/daily/[date]/route.ts
+++ app/api/water/daily/[date]/route.ts
@@ -1,40 +1,23 @@
 import { NextRequest, NextResponse } from 'next/server';
-// import { getServerSession } from 'next-auth';
-import { endOfDay, startOfDay } from 'date-fns';
-// import authOptions from '@/configs/next-auth';
+import { getServerSession } from 'next-auth';
+import authOptions from '@/configs/next-auth';
 import prisma from '@/configs/prisma';
 
 export const GET = async (req: NextRequest, { params }: { params: { date: string } }) => {
-    // const session = await getServerSession(authOptions);
+    const session = await getServerSession(authOptions);
 
-    // console.log(session);
+    console.log(session);
 
-    // if (!session) {
-    //     return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
-    // }
+    if (!session) {
+        return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+    }
 
     try {
-        const { date } = params;
-
-        console.log(date);
-
-        const startDate = startOfDay(new Date(date)).toISOString();
-        const endDate = endOfDay(new Date(date)).toISOString();
-
-        console.log('startDate', startDate);
-        console.log('endDate', endDate);
-
         const dailyWaters = await prisma.water.findMany({
             where: {
-                // userId: session.user.id,
-                date: {
-                    gte: startDate,
-                    lte: endDate,
-                },
+                userId: session.user.id,
             },
         });
-
-        console.log(dailyWaters);
 
         return NextResponse.json(dailyWaters, { status: 200 });
     } catch (error) {
